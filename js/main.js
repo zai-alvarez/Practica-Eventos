9f222fe2224f13197fa620a1f19ae015ed7cf077
zai-alvarez <zai.alvarez.l@gmail.com>
2023-02-23 19:35:30 +0100

    estructure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const name = document.querySelector('.js_name');
+const lastName = document.querySelector('.js_lastName');
+const profession = document.querySelector('.js_profession');
+const button = document.querySelector('.js_button');
+
+button.addEventListener("click", () => {
+
+});
